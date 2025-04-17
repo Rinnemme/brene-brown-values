a67b14bf304efd9d26e0c3ccfc6c6a6874011c4e
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-04-17 17:46:53 -0400

    Update main assessment page to move between the two current stages one-way

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import EarlyPriorities from "@/src/components/earlyPriorities";
+import Comparison from "@/src/components/comparisonComponent";
+import { useSelector } from "react-redux";
+import type { AppState } from "@/src/store/store";
 
 export default function Home() {
+  const assessmentStage = useSelector(
+    (state: AppState) => state.assessmentStage
+  );
   return (
-    <div className="w-full px-8 py-12 flex align-center justify-center">
-      <EarlyPriorities />
+    <div className="w-full px- py-12 flex flex-col items-center justify-center mb-20">
+      {assessmentStage === 0 && <EarlyPriorities />}
+      {assessmentStage === 1 && <Comparison />}
     </div>
   );
 }
