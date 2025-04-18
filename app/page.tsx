bb3e1c17cf96fe02286bd208ae369a5b5a8847bc
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-04-18 17:34:11 -0400

    Add assessment intro as landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import AssessmentIntro from "@/src/components/assessmentIntro";
 
 export default function Home() {
-  return <div className="">lol</div>;
+  return (
+    <div className="w-full h-fit min-h-dvh fade-in absolute px-2 flex flex-col items-center justify-center pt-28 pb-28 sm:pt-32 sm:pb-32">
+      <AssessmentIntro />
+    </div>
+  );
 }
