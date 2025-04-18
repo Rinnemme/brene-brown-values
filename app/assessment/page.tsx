bf097373bbfdd26ebb4879f9633df14602a45d3f
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-04-18 17:36:39 -0400

    Add new component flow to assessment page, and repair broken styling in mobile

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -4,15 +4,17 @@ import EarlyPriorities from "@/src/components/earlyPriorities";
 import Comparison from "@/src/components/comparisonComponent";
 import { useSelector } from "react-redux";
 import type { AppState } from "@/src/store/store";
+import Results from "@/src/components/finalResults";
 
 export default function Home() {
   const assessmentStage = useSelector(
     (state: AppState) => state.assessmentStage
   );
   return (
-    <div className="w-full px- py-12 flex flex-col items-center justify-center mb-20">
+    <div className="w-full h-fit min-h-dvh fade-in absolute px-2 flex flex-col items-center justify-center pt-28 pb-28 sm:pt-32 sm:pb-32">
       {assessmentStage === 0 && <EarlyPriorities />}
       {assessmentStage === 1 && <Comparison />}
+      {assessmentStage === 2 && <Results />}
     </div>
   );
 }
