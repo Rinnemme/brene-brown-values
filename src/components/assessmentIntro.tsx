7bc4422e725eb8e91bb03c1a52fc4cabac93445f
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-04-18 20:23:12 -0400

    update assessment intro verbiage

--- src/components/assessmentIntro.tsx
+++ src/components/assessmentIntro.tsx
@@ -6,11 +6,11 @@ export default function AssessmentIntro() {
   return (
     <div className="fade-in max-w-5xl flex flex-col items-center justify-center text-balance px-8">
       <div className="text-3xl sm:text-4xl w-full text-center mb-8 merriweather">
-        What Matters Most to You?
+        What Do You Value Most?
       </div>
       <p className="text-sm sm:text-base text-center text-balance max-w-lg">
         This assessment will cause you to think carefully about your priorities,
-        and illuminate what you believe and hold important among Brené Brown's{" "}
+        and what values you consider most important among Brené Brown's{" "}
         <a
           target="_blank"
           className="text-orange-700 hover:cursor-pointer hover:underline hover:underline-offset-4"
@@ -19,11 +19,12 @@ export default function AssessmentIntro() {
           List of Values
         </a>
         . <br></br>
-        <br></br>You will first be presented with the full list of values, among
-        which you can select as many as you feel are personally important to you
-        in some capacity. <br></br>
-        <br></br>Then, among those you have chosen, you will narrow down your
-        priorities by evaluating your chosen values two values at a time.
+        <br></br>First, you will be presented with the full list of values,
+        among which you can select as many as you feel are personally important
+        to you, without concern for ordering.<br></br>
+        <br></br>Then, you will be prompted to choose which is more important
+        between two of your selected values at a time, until your personal
+        values are completely sorted.
       </p>
       <Link
         className="mt-10 py-2 px-3 rounded-lg text-white bg-stone-900 hover:bg-orange-700 transition-all duration-300 hover:cursor-pointer disabled:bg-stone-400 disabled:hover:bg-stone-400 disabled:hover:cursor-default"
